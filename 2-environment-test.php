dceea22f6001fcb1503d9626565e6bb3d189f794
Zenlac <caleleizengrillera@gmail.com>
2023-10-04 21:11:07 +0800

    fixed some issues

---
+++ 2-environment-test.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Envi testing</title>
+</head>
+<body>
+    <?php echo "Do I work yet? hi there";?>
+    <?php echo "Please do "?>
+</body>
+</html>
