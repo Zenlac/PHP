182f4bd4708b5297761c20c7aef55ff940dfc15d
Zenlac <caleleizengrillera@gmail.com>
2023-10-04 21:34:06 +0800

    Learning more about syntax

--- 3-syntax-overview.php
+++ 3-syntax-overview.php
@@ -3,10 +3,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Envi testing</title>
+    <title>Document</title>
 </head>
 <body>
-    <?php echo "Do I work yet? hi there";?>
-    <?php echo "Work already please "?>
+    
 </body>
 </html>
