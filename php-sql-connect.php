e8c42c48433d4fc80887f3a0581e3454d4327268
Zenlac <caleleizengrillera@gmail.com>
2023-11-15 20:10:58 +0800

    continued unfinished parts

--- php-sql-connect.php
+++ php-sql-connect.php
@@ -12,12 +12,13 @@
     }
     //else
     //    echo "Connected Successfully";
-
-    $sql="Select * from Student";
-    $result=$conn->query("$sql");
+    
 ?>
     <table>
         <?php
+        $sql="Select * from Student";
+        $result=$conn->query("$sql");
+
         if($result){
             while ($row=$result->fetch_assoc()){
                 echo "ID Number: " .$row["StudentID"] ."<br>"
@@ -28,53 +29,59 @@
                 ."Phone Number: " .$row["Phone"] ."<br>"
                 ."<br>";
             }
-    }
+        }
         ?>
     </table>
 
     <table>
         <?php
-        $sql="Select * from Course";
-        $result=$conn->query("$sql");
+            $sql="Select * from Course";
+            $result=$conn->query("$sql");
 
-        if($result){
-            while ($row=$result->fetch_assoc()){
-                echo "Course ID: " .$row["CourseID"] ."<br>"
-                ."Course Name: " .$row["CourseName"] ."<br>"
-                ."Credits: " .$row["Credits"] ."<br>"
-                ."<br>";
+            if($result){
+                while ($row=$result->fetch_assoc()){
+                    echo "Course ID: " .$row["CourseID"] ."<br>"
+                    ."Course Name: " .$row["CourseName"] ."<br>"
+                    ."Credits: " .$row["Credits"] ."<br>"
+                    ."<br>";
+                }
             }
-        }
         ?>
     </table>
 
-    $sql="Select * from Instructor";
-    $result=$conn->query("$sql");
+    <table>
+        <?php
+            $sql="Select * from Instructor";
+            $result=$conn->query("$sql");
+        
+            if($result){
+                while ($row=$result->fetch_assoc()){
+                    echo "Instructor ID: " .$row["InstructorID"] ."<br>"
+                    ."First Name: " .$row["FirstName"] ."<br>"
+                    ."Last Name: " .$row["LastName"] ."<br>"
+                    ."Email: " .$row["Email"] ."<br>"
+                    ."Phone Number: " .$row["Phone"] ."<br>"
+                    ."<br>";
+                }
+            }
+        ?>
+    </table>
 
-    if($result){
-        while ($row=$result->fetch_assoc()){
-            echo "Instructor ID: " .$row["InstructorID"] ."<br>"
-            ."First Name: " .$row["FirstName"] ."<br>"
-            ."Last Name: " .$row["LastName"] ."<br>"
-            ."Email: " .$row["Email"] ."<br>"
-            ."Phone Number: " .$row["Phone"] ."<br>"
-            ."<br>";
-        }
-    }
+    <table>
+        <?php
+            $sql="Select * from Enrollment";
+            $result=$conn->query("$sql");
+        
+            if($result){
+                while ($row=$result->fetch_assoc()){
+                    echo "Enrollment ID: " .$row["EnrollmentID"] ."<br>"
+                    ."Student ID: " .$row["StudentID"] ."<br>"
+                    ."Course ID: " .$row["CourseID"] ."<br>"
+                    ."Enrollment Date: " .$row["EnrollmentDate"] ."<br>"
+                    ."Grade: " .$row["Grade"] ."<br>"
+                    ."<br>";
+                }
+            }
+        ?>
+    </table>
 
-    $sql="Select * from Enrollment";
-    $result=$conn->query("$sql");
-
-    if($result){
-        while ($row=$result->fetch_assoc()){
-            echo "Enrollment ID: " .$row["EnrollmentID"] ."<br>"
-            ."Student ID: " .$row["StudentID"] ."<br>"
-            ."Course ID: " .$row["CourseID"] ."<br>"
-            ."Enrollment Date: " .$row["EnrollmentDate"] ."<br>"
-            ."Grade: " .$row["Grade"] ."<br>"
-            ."<br>";
-        }
-    }
-
-
-?>
