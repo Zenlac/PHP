dbbe1c65c548cf756faa67705f178ea404188e80
Zenlac <caleleizengrillera@gmail.com>
2023-11-15 18:09:41 +0800

    Did lab php and sql connecting

---
+++ php-sql-connect.php
@@ -0,0 +1,72 @@
+
+<?php
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "StudentRecord";
+
+    $conn = new mysqli($servername,$username,$password,$dbname);
+    if ($conn-> connect_error){
+        die("Connection Failed.".$conn->connect_error);
+
+    }
+    //else
+    //    echo "Connected Successfully";
+
+    $sql="Select * from Student";
+    $result=$conn->query("$sql");
+    
+    if($result){
+        while ($row=$result->fetch_assoc()){
+            echo "ID Number: " .$row["StudentID"] ."<br>"
+            ."First Name: " .$row["FirstName"] ."<br>"
+            ."Last Name: " .$row["LastName"] ."<br>"
+            ."Birthday: " .$row["DateOfBirth"] ."<br>"
+            ."Email: " .$row["Email"] ."<br>"
+            ."Phone Number: " .$row["Phone"] ."<br>"
+            ."<br>";
+        }
+    }
+
+    $sql="Select * from Course";
+    $result=$conn->query("$sql");
+
+    if($result){
+        while ($row=$result->fetch_assoc()){
+            echo "Course ID: " .$row["CourseID"] ."<br>"
+            ."Course Name: " .$row["CourseName"] ."<br>"
+            ."Credits: " .$row["Credits"] ."<br>"
+            ."<br>";
+        }
+    }
+
+    $sql="Select * from Instructor";
+    $result=$conn->query("$sql");
+
+    if($result){
+        while ($row=$result->fetch_assoc()){
+            echo "Instructor ID: " .$row["InstructorID"] ."<br>"
+            ."First Name: " .$row["FirstName"] ."<br>"
+            ."Last Name: " .$row["LastName"] ."<br>"
+            ."Email: " .$row["Email"] ."<br>"
+            ."Phone Number: " .$row["Phone"] ."<br>"
+            ."<br>";
+        }
+    }
+
+    $sql="Select * from Enrollment";
+    $result=$conn->query("$sql");
+
+    if($result){
+        while ($row=$result->fetch_assoc()){
+            echo "Enrollment ID: " .$row["EnrollmentID"] ."<br>"
+            ."Student ID: " .$row["StudentID"] ."<br>"
+            ."Course ID: " .$row["CourseID"] ."<br>"
+            ."Enrollment Date: " .$row["EnrollmentDate"] ."<br>"
+            ."Grade: " .$row["Grade"] ."<br>"
+            ."<br>";
+        }
+    }
+
+
+?>
