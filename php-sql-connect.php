895f7dd2c2b82a1675bf99938eb55bec89da0d61
Zenlac <caleleizengrillera@gmail.com>
2023-11-15 18:36:31 +0800

    continue at home

--- php-sql-connect.php
+++ php-sql-connect.php
@@ -15,30 +15,38 @@
 
     $sql="Select * from Student";
     $result=$conn->query("$sql");
-    
-    if($result){
-        while ($row=$result->fetch_assoc()){
-            echo "ID Number: " .$row["StudentID"] ."<br>"
-            ."First Name: " .$row["FirstName"] ."<br>"
-            ."Last Name: " .$row["LastName"] ."<br>"
-            ."Birthday: " .$row["DateOfBirth"] ."<br>"
-            ."Email: " .$row["Email"] ."<br>"
-            ."Phone Number: " .$row["Phone"] ."<br>"
-            ."<br>";
-        }
+?>
+    <table>
+        <?php
+        if($result){
+            while ($row=$result->fetch_assoc()){
+                echo "ID Number: " .$row["StudentID"] ."<br>"
+                ."First Name: " .$row["FirstName"] ."<br>"
+                ."Last Name: " .$row["LastName"] ."<br>"
+                ."Birthday: " .$row["DateOfBirth"] ."<br>"
+                ."Email: " .$row["Email"] ."<br>"
+                ."Phone Number: " .$row["Phone"] ."<br>"
+                ."<br>";
+            }
     }
+        ?>
+    </table>
 
-    $sql="Select * from Course";
-    $result=$conn->query("$sql");
+    <table>
+        <?php
+        $sql="Select * from Course";
+        $result=$conn->query("$sql");
 
-    if($result){
-        while ($row=$result->fetch_assoc()){
-            echo "Course ID: " .$row["CourseID"] ."<br>"
-            ."Course Name: " .$row["CourseName"] ."<br>"
-            ."Credits: " .$row["Credits"] ."<br>"
-            ."<br>";
+        if($result){
+            while ($row=$result->fetch_assoc()){
+                echo "Course ID: " .$row["CourseID"] ."<br>"
+                ."Course Name: " .$row["CourseName"] ."<br>"
+                ."Credits: " .$row["Credits"] ."<br>"
+                ."<br>";
+            }
         }
-    }
+        ?>
+    </table>
 
     $sql="Select * from Instructor";
     $result=$conn->query("$sql");
